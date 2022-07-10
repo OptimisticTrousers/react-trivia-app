4a9a45699d20f06e9544424da3b2a25e8a90ffd2
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-10 16:08:11 -0400

    Deleting unnecessary styles

--- src/components/ui/Question.js
+++ src/components/ui/Question.js
@@ -13,14 +13,6 @@ export default function Question({question, selectQuestion, userSelections, isQu
 
     const allAnswers = userSelections.map((answerData) => {
 
-        const regularStyle = {
-            backgroundColor: answerData.isSelected ? "#D6DBF5" : "#F5F7FB"
-        }
-
-        const quizEndStyle = {
-            backgroundColor: answerData.isSelected && answerData.correct ? "#94D7A2" : "#F8BCBC"
-        }
-
         return <button key={uniqid()} style={determineStyle(answerData)} className="answer-button" onClick={() => selectQuestion({question, answer: answerData.answer, isSelected: answerData.isSelected })}>{answerData.answer}</button>
     })
     
