281b22043929102cd4b7ce0f313a40b95aef1bb7
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-10 17:15:33 -0400

    Decoding JSON response using a library called 'html-entities' in my App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Start from './components/Start'
 import Quiz from './components/Quiz'
 import uniqid from 'uniqid';
+import {decode} from 'html-entities';
 
 export default function App() {
     
@@ -10,7 +11,15 @@ export default function App() {
     const [questions, setQuestions] = React.useState([])
     
     React.useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5&type=multiple').then(res => res.json()).then(data => setQuestions(data.results))
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple').then(res => res.json()).then(data => {
+            const array = data.results
+            console.log(array)
+
+            const decodedArray = array.map(question => {
+                return {question: decode(question.question), correct_answer: decode(question.correct_answer), incorrect_answers: question.incorrect_answers.map(incorrectAnswer => decode(incorrectAnswer))}
+            })
+            setQuestions(decodedArray)
+        })
     }, [isQuizOver])
     
     function handleStart(){
