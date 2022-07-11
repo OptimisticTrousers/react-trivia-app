ca89578200af05df93fe155bb3f33f40b1bc4275
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-11 07:42:29 -0400

    Updating start text

--- src/components/Start.js
+++ src/components/Start.js
@@ -4,8 +4,8 @@ import Button from './ui/Button'
 export default function Start({handleStart}){
     return (
         <div className="start">
-            <h1 className="start-title">Quizzical</h1>
-            <p className="start-description">Some description if needed</p>
+            <h1 className="start-title">Optimistic Quizzal</h1>
+            <p className="start-description">Test your trivia knowledge right here!</p>
             <Button handleClick={handleStart} value={"Start quiz"}/>
         </div>
     )
